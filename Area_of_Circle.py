7009845718c6cab9238fe19e3f1895a39dd8fe06
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-03-11 09:59:09 +0100

    Create Area_of_Circle.py

---
+++ Area_of_Circle.py
@@ -0,0 +1,10 @@
+# By INNAN Nouhaila 
+# Compute the area of a circle, given the length of its radius.
+
+PI = 3.14
+radius = 12
+
+# Circle area formula
+area = PI * radius ** 2
+
+print("A circle with a radius of " + str(radius) + " inches has an area of " + str(area) + " square inches.") 
