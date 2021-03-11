28703e62863c9b9fc42bd642577132c0e0e7afe7
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-03-11 09:56:37 +0100

    Create Circumference_of_Circle.py

---
+++ Circumference_of_Circle.py
@@ -0,0 +1,10 @@
+# By INNAN Nouhaila
+# Compute the circumference of a circle, given the length of its radius. 
+
+PI = 3.14
+radius = 12
+
+# Circle circumference formula
+circumference = 2 * PI * radius
+
+print("A circle with a radius of " + str(radius) + " inches has a circumference of " + str(circumference) + " inches.")
