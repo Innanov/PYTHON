e8243332b46d02115e30a56845b10ba3f55b0c13
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-03-11 09:53:35 +0100

    Create Area_of_rectangle.py

---
+++ Area_of_rectangle.py
@@ -0,0 +1,10 @@
+# By INNAN Nouhaila
+# Compute the area of a rectangle, given its width and height.
+
+width = 5
+height = 9
+
+# Rectangle area formula
+area = width * height
+
+print("A rectangle " + str(width) + " inches wide and " + str(height) + " inches high has an area of " + str(area) + " square inches.")
