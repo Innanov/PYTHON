c9c788ab7064797b31a91b4e1fe072f4704d20f0
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-03-11 09:50:53 +0100

    Create Perimeter_of_Rectangle.py

---
+++ Perimeter_of_Rectangle.py
@@ -0,0 +1,10 @@
+# By INNAN Nouhaila
+#Compute the length of a rectangle's perimeter, given its width and height.
+
+width = 4
+height = 7
+
+# Rectangle perimeter formula
+perimeter = width * 2 + height * 2
+
+print("A rectangle " + str(width) + " inches wide and " + str(height) + " inches high has a perimeter of " + str(perimeter) + " inches.")
