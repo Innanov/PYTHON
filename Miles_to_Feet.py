f1bacd05910afdf593bcf56fc51a1fa125de44f0
INNAN Nouhaila <64653897+Innanov@users.noreply.github.com>
2021-03-11 09:43:22 +0100

    Create Miles_to_Feet.py

---
+++ Miles_to_Feet.py
@@ -0,0 +1,7 @@
+#INNAN Nouhaila
+# Compute the number of feet corresponding to a number of miles.
+#For example let's convert 13 miles to feet 
+miles = 13
+# Miles to feet conversion formula
+feet = miles * 5280
+print(str(miles) + " miles equals " + str(feet) + " feet.")
